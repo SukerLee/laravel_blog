ac6335aefd9edb5745a579acb012c91e87a70e8d
Suker <suker040609@gmail.com>
2017-05-24 18:08:40 +0800

    5/24

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="style/css/ch-ui.admin.css">
-	<link rel="stylesheet" href="style/font/css/font-awesome.min.css">
-	<script type="text/javascript" src="style/js/jquery.js"></script>
-    <script type="text/javascript" src="style/js/ch-ui.admin.js"></script>
-</head>
-<body>
-	<!--头部 开始-->
+@extends('layouts.admin')
+@section('content')
+    <!--头部 开始-->
 	<div class="top_box">
 		<div class="top_left">
 			<div class="logo">后台管理模板</div>
@@ -21,13 +13,12 @@
 			<ul>
 				<li>管理员：admin</li>
 				<li><a href="pass.html" target="main">修改密码</a></li>
-				<li><a href="#">退出</a></li>
+				<li><a href="<?= url('admin/logout')?>">退出</a></li>
 			</ul>
 		</div>
 	</div>
 	<!--头部 结束-->
-
-	<!--左侧导航 开始-->
+<!--左侧导航 开始-->
 	<div class="menu_box">
 		<ul>
             <li>
@@ -61,7 +52,7 @@
 
 	<!--主体部分 开始-->
 	<div class="main_box">
-		<iframe src="info.html" frameborder="0" width="100%" height="100%" name="main"></iframe> 
+		<iframe src="{{url('admin/info')}}" frameborder="0" width="100%" height="100%" name="main"></iframe> 
 	</div>
 	<!--主体部分 结束-->
 
@@ -70,5 +61,8 @@
 		CopyRight © 2016. Powered By <a href="http://www.chenhua.club">http://www.chenhua.club</a>.
 	</div>
 	<!--底部 结束-->
-</body>
-</html>
+@endsection
+
+	
+
+	
